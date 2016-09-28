135e4e5469304b3e6dd630b13ad5152f9a9bab6c
Nils Neumann <neumann@placeworkers.com>
2016-09-28 16:47:04 +0200

    added xmlsitemap

--- site/config/config.php
+++ site/config/config.php
@@ -28,3 +28,48 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+/*
+
+---------------------------------------
+Multi-Language support setup
+---------------------------------------
+
+If you want to run a site with multiple languages,
+enable support for it here. As soon as you set
+
+c::set('lang.support', true);
+
+Kirby will automatically create language-dependent
+URLs like:
+
+http://yourdomain.com/en/blog
+
+or
+
+http://yourdomain.com/de/blog
+
+Make sure to set the default language code and
+also the available language codes.
+
+If you keep…
+
+c::set('lang.detect', true);
+
+Kirby will try to detect the default language
+from the user agent string instead of using the
+default language.
+
+*/
+
+c::set('languages', array(
+  array(
+    'code'    => 'de',
+    'name'    => 'Deutsch',
+    'default' => true,
+    'locale'  => 'de_DE',
+    'url'     => '/',
+  )
+));
+
+c::set('language.detect', true);
