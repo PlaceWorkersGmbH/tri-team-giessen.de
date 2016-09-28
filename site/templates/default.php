5874a4d8a6295c502a2f7d4d847870af79973f25
Nils Neumann <neumann@placeworkers.com>
2016-09-28 19:44:14 +0200

    moved submenu to own snippet

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,26 +3,7 @@
 
   <main class="main" role="main">
 
-    <?php if ($page->depth() == 1 && $page->hasVisibleChildren()): ?>
-      <?php go($page->children()->first()); ?>
-    <?php endif ?>
-
-    <?php if ($page->depth() > 1) : ?>
-      <?php $p = $page->parent(); ?>
-    <?php else: ?>
-      <?php $p = $page; ?>
-    <?php endif ?>
-
-    <?php if($p->hasVisibleChildren()): ?>
-    <ul class="submenu">
-      <?php foreach($p->children()->visible() as $p): ?>
-      <li>
-        <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-      </li>
-      <?php endforeach ?>
-    </ul>
-    <?php endif ?>
-
+    <?php snippet('submenu') ?>
 
     <div class="text">
       <h1><?php echo $page->title()->html() ?></h1>
