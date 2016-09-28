c9a499bd4bb9d761b8deb9b26016daf8600c73b1
Nils Neumann <neumann@placeworkers.com>
2016-09-28 18:53:28 +0200

    always nav to first visible child

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,17 +3,25 @@
 
   <main class="main" role="main">
 
+    <?php if ($page->depth() == 1 && $page->hasVisibleChildren()): ?>
+      <?php go($page->children()->first()); ?>
+    <?php endif ?>
 
+    <?php if ($page->depth() > 1) : ?>
+      <?php $p = $page->parent(); ?>
+    <?php else: ?>
       <?php $p = $page; ?>
-      <?php if($p->hasVisibleChildren()): ?>
-      <ul class="submenu">
-        <?php foreach($p->children()->visible() as $p): ?>
-        <li>
-          <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-      <?php endif ?>
+    <?php endif ?>
+
+    <?php if($p->hasVisibleChildren()): ?>
+    <ul class="submenu">
+      <?php foreach($p->children()->visible() as $p): ?>
+      <li>
+        <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+      </li>
+      <?php endforeach ?>
+    </ul>
+    <?php endif ?>
 
 
     <div class="text">
