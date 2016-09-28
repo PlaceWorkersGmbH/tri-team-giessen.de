abf159b3b6b6f5efe27664068df7008af9f355ab
Nils Neumann <neumann@placeworkers.com>
2016-09-28 16:28:45 +0200

    users

---
+++ site/accounts/denis.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: denis
+password: >
+  $2a$10$oheRXVKA2.8EwPw7JkHnVOq0eElDaXX4g6hVfHylBo5Cdmk9I22z6
+email: schikunow@placeworkers.com
+language: de
+role: admin
---
+++ site/accounts/nils.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: nils
+firstname: Nils
+lastname: ""
+email: neumann@placeworkers.com
+password: >
+  $2a$10$Wm8I3BF/iAG5FD/9UMfag.9j59r4Ah6PMD3WxOJwwQ6Zk6Fecwyg.
+language: en
+role: admin
