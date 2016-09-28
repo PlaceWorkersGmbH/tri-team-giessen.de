5790a5f73837182bf0177b8e9f5b301358116621
Nils Neumann <neumann@placeworkers.com>
2016-09-28 18:40:54 +0200

    added temp submenu

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,6 +3,19 @@
 
   <main class="main" role="main">
 
+
+      <?php $p = $page; ?>
+      <?php if($p->hasVisibleChildren()): ?>
+      <ul class="submenu">
+        <?php foreach($p->children()->visible() as $p): ?>
+        <li>
+          <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+        </li>
+        <?php endforeach ?>
+      </ul>
+      <?php endif ?>
+
+
     <div class="text">
       <h1><?php echo $page->title()->html() ?></h1>
       <?php echo $page->text()->kirbytext() ?>
