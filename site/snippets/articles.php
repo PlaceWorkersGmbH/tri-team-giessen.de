794aaf6fe303192195cb191b7fff452060a07d30
Nils Neumann <neumann@placeworkers.com>
2016-10-06 12:08:28 +0200

    placeholder-img

--- site/snippets/articles.php
+++ site/snippets/articles.php
@@ -3,11 +3,14 @@
 <ul class="teaser cf">
   <?php foreach($articles as $project): ?>
   <li>
+      <a class="teaser-img" href="<?php echo $project->url() ?>">
       <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
-        <a class="teaser-img" href="<?php echo $project->url() ?>">
         <?= thumb($image, array('width' => 700, 'height' => 420, 'crop' => true)) ?>
-        </a>
+      <?php else : ?>
+        <?php $image = $page->image("img-placeholder.png"); ?>
+        <?= thumb($image, array('width' => 700, 'height' => 420, 'crop' => true)) ?>
       <?php endif ?>
+      </a>
       <h5><?= $project->date('d.m.Y') ?></h5>
       <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
       <p><?php echo $project->text()->excerpt(200) ?> <a href="<?php echo $project->url() ?>">weiterlesen&nbsp;→</a></p>
